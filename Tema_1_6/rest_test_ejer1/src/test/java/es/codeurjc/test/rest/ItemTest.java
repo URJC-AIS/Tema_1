666309f435ea2140deacd3f2db835cf8997a7ce7
Maes95 <michel.maes95@gmail.com>
2023-01-26 22:22:57 +0100

    Updates examples T1.5-1.6

--- Tema_1_6/rest_test_ejer1/src/test/java/es/codeurjc/test/rest/ItemTest.java
+++ Tema_1_6/rest_test_ejer1/src/test/java/es/codeurjc/test/rest/ItemTest.java
@@ -11,7 +11,7 @@ import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
